b458285d079f589d11ec76a6905bbb3c6ce96a47
Iarlen <iarlensr@hotmail.com>
2023-10-06 23:58:16 -0300

    refactor: modifying styles and some properties of the LoginButton composition.

--- src/components/LoginButton/LoginButtonRoot.tsx
+++ src/components/LoginButton/LoginButtonRoot.tsx
@@ -4,15 +4,19 @@ import styles from '@/components/LoginButton/LoginButton.module.css'
 interface LoginButtonRootProps extends ComponentProps<'button'> {
   children: React.ReactNode
   socialMethod: 'google' | 'github'
+  onClick: () => void
 }
-const LoginButtonRoot = ({ children, ...props }: LoginButtonRootProps) => {
+const LoginButtonRoot = ({
+  children,
+  onClick,
+  socialMethod,
+  ...props
+}: LoginButtonRootProps) => {
   const choiceStyled =
-    props.socialMethod === 'google'
-      ? styles.login__google
-      : styles.login__github
+    socialMethod === 'google' ? styles.login__google : styles.login__github
 
   return (
-    <button {...props} className={choiceStyled}>
+    <button {...props} onClick={onClick} className={choiceStyled}>
       {children}
     </button>
   )
